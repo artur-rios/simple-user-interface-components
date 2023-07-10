7d22af27551b869bf48baddd268739ba6562bbc0
Artur Rios <arturdev@duck.com>
2023-07-10 10:52:57 -0300

    build: update bundle name

--- framework-agnostic/webpack.config.js
+++ framework-agnostic/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.min.js",
+    filename: "suic.min.js",
     path: path.resolve(__dirname, "dist/js"),
   },
 };
